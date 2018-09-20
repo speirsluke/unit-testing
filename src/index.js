8a97f884f2f2b3e584ffec940372a1a2d0d7178d
Luke Speirs <Luke@Lukes-MacBook-Pro.local>
2018-09-20 09:47:52 +0100

    compelted some questions

--- src/index.js
+++ src/index.js
@@ -3,3 +3,103 @@ function add(a, b){
 }
 
 exports.add = add;
+
+function longestString(arr) {
+let max = 0;
+let wordLengths = arr.map(function(word){
+  return word.length;
+})
+wordLengths.map(function(number){
+  if (number > max) {
+    max = number;
+  }
+ return max;
+})
+return arr[wordLengths.indexOf(max)]; 
+}
+
+exports.longestString = longestString;
+
+function replaceLetter(string) {
+  
+let stringArray = Array.from(string); 
+let newStringArray = stringArray.map(function(letter){
+  if ((letter == 'i') || (letter == 'l')) {
+    letter = '1'
+    return letter
+  } else if (letter == 'z') {
+    letter = '2';
+    return letter
+  } else if (letter == 'e') {
+    letter = '3'
+    return letter
+  } else if (letter == 'a') {
+    letter = '4'
+    return letter
+  } else if (letter == 's') {
+    letter = '5'
+    return letter
+  } else if (letter == 'g') {
+    letter = '6'
+    return letter
+  } else if ((letter == 't') || (letter == 'y')) {
+    letter = '7'
+    return letter
+  }  else if (letter == 'b') {
+    letter = '8'
+    return letter
+  }  else if (letter == 'q') {
+    letter = '9'
+    return letter
+  } else if (letter == 'o') {
+    letter = '0'
+    return letter
+  } else {
+  return letter 
+  }
+})
+
+  return newStringArray.join('')
+}
+
+exports.replaceLetter = replaceLetter;
+
+function uniqueArray(array) {
+let newArray = [];
+  array.forEach(function(word){
+  if (newArray.indexOf(word) === -1){
+    newArray.push(word)
+  }
+})
+return newArray;
+}
+
+exports.uniqueArray = uniqueArray;
+
+
+function Developer(name, language){
+this.name = name;
+this.language = language;
+}
+
+
+
+
+
+const newLang = {
+language: [],
+learnLanguage(newLanguage) {
+  if (this.language.indexOf(newLanguage) === -1) {
+    this.language.push(newLanguage);
+  }
+}
+}
+
+Developer.prototype = newLang;
+exports.Developer = Developer;
+exports.newLang = newLang; 
+exports.newLang.learnLanguage = learnLanguage;
+
+
+
+
--- test/index.test.js
+++ test/index.test.js
@@ -5,3 +5,55 @@ test('Addition', function(){
   const result = functions.add(2, 3);
   expect(result).toBe(expected);
 });
+
+test('Negative Addition', function(){
+  const expected = 2;
+  const result = functions.add(-1, 3);
+  expect(result).toBe(expected);
+});
+
+test('Longest String', function() {
+  const expected = "Anthony";
+  const result = functions.longestString(["Luke", "Jamal", "Claire", "Anthony"])
+  expect(result).toBe(expected);
+} )
+
+test('Longest String', function() {
+  const expected = "Maximillian";
+  const result = functions.longestString(["Luke", "Jamal", "Claire", "Anthony", "Maximillian"])
+  expect(result).toBe(expected);
+} )
+
+test('Replace Letter', function() {
+  const expected = '5ch001';
+  const result = functions.replaceLetter('school')
+expect(result).toBe(expected);
+})
+
+test('Unique Array', function() {
+  const expected = ['dog', 'cat', 'bird']; 
+  const result = functions.uniqueArray(['dog', 'cat', 'dog', 'cat', 'bird'])
+expect(result).toEqual(expected);
+})
+
+test("constructor", function(){
+  const expected = {
+    name: "luke",
+    language: ["ruby", "javascript"]
+  };
+
+  const luke = new functions.Developer("luke", ["ruby", "javascript"])
+  
+  expect(luke).toEqual(expected)
+})
+
+test("learn language", function(){
+  const expected = {
+    name: "jamal",
+    language: ["ruby", "javascript", "python"]
+  };
+  const result = expected.learnLanguage('python');
+
+  expect(result).toEqual(expected)
+})
+
